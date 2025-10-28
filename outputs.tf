d8f6d80d2ac3c66c50efb37a526371c91dba74e3
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-28 14:16:50 -0500

    asd

--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "site_url" {
-  value       = "https://${ibm_cos_bucket.vibe.bucket_name}.${var.region}.cloud-object-storage.appdomain.cloud"
-  description = "Public website URL for the deployed Vibe IDE app"
-}
